2c6869590421a28c99002f63e448ab01e0e3246a
Danny Cheun <dcheun@gmail.com>
2020-09-12 06:14:56 -0700

    feat: Adds MeaningCloud API data
    Adds dotenv package to access environment vars for API data.
    Read in API vars to server.

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,29 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require("dotenv");
+dotenv.config();
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-console.log(__dirname)
+var textapi = {
+  application_key: process.env.API_KEY,
+};
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+console.log(__dirname);
+
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+  console.log("Example app listening on port 8080!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
